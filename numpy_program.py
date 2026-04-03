3dfdf8fa83d3745a579e775ba888982dd55314ff
sravanthib880 <sravanthib880@gmail.com>
2026-04-03 11:53:55 +0530

    Add files via upload

---
+++ numpy_program.py
@@ -0,0 +1,15 @@
+import numpy as np
+# Create Numpy arrys
+array1 = np.array([1,2,3])
+array2 = np.array([4,5,6])
+# addition
+addition = array1 + array2
+# multiplication
+multiplication = array1 * array2
+
+# Display results
+print("Array 1:", array1)
+print("Array 2:", array2)
+print("Addition:", addition)
+print("Multiplication:", multiplication)
+
